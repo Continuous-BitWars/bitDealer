ca764e0314b9cbaea0d6489a9c746c3c7c88f990
Andreas Heinrich <git@heinrian.com>
2024-05-01 14:21:36 +0200

    move ExampleResource.java

--- src/main/java/de/bitwars/api/resources/ExampleResource.java
+++ src/main/java/de/bitwars/api/resources/ExampleResource.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.bitwars;
+package de.bitwars.api.resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;

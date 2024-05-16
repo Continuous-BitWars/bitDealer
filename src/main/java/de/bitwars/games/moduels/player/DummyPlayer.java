93a97908141ccaa56126a7236d0b4658bc5ea59f
Andreas Heinrich <git@heinrian.com>
2024-05-17 00:23:17 +0200

    fix DummyPlayer

--- src/main/java/de/bitwars/games/moduels/player/DummyPlayer.java
+++ src/main/java/de/bitwars/games/moduels/player/DummyPlayer.java
@@ -4,6 +4,8 @@ import de.bitwars.games.moduels.ActionProvider;
 import de.bitwars.games.moduels.BaseBU;
 import de.bitwars.games.moduels.GameBU;
 import de.bitwars.games.moduels.PlayerActionBU;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Random;
@@ -11,6 +13,7 @@ import java.util.Random;
 
 public class DummyPlayer implements ActionProvider {
 
+    private static final Logger log = LoggerFactory.getLogger(DummyPlayer.class);
     private final long id;
     private final String color;
 
@@ -44,12 +47,19 @@ public class DummyPlayer implements ActionProvider {
     public List<PlayerActionBU> requestStep(GameBU gameBU) {
         if (this.id >= 1001 && this.id <= 1010) {
             Random rand = new Random();
-            List<BaseBU> ownBases = gameBU.getGameField().getBases().values().stream().filter(baseBU -> baseBU.getPlayerId() == this.id).toList();
+            log.debug("requestStep {} -> 01", this.id);
+            List<BaseBU> ownBases = gameBU.getGameField().getBases().values().stream().filter(baseBU -> baseBU.getPlayerId() == this.id && baseBU.getPopulation() > 2).toList();
+            log.debug("requestStep {} -> 02", this.id);
             List<BaseBU> otherBases = gameBU.getGameField().getBases().values().stream().filter(baseBU -> baseBU.getPlayerId() != this.id).toList();
+            log.debug("requestStep {} -> 03", this.id);
             if (!ownBases.isEmpty() && !otherBases.isEmpty()) {
+                log.debug("requestStep {} -> 04", this.id);
                 BaseBU baseSource = ownBases.get(rand.nextInt(ownBases.size()));
+                log.debug("requestStep {} -> 05", this.id);
                 BaseBU baseTarget = otherBases.get(rand.nextInt(otherBases.size()));
+                log.debug("requestStep {} -> 06", this.id);
                 int sendCount = rand.nextInt(baseSource.getPopulation() - 1);
+                log.debug("requestStep {} -> 07", this.id);
 
                 return List.of(new PlayerActionBU(baseSource.getUid(), baseTarget.getUid(), sendCount));
 

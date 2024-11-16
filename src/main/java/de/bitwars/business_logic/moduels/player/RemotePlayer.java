866a2b846f7947881988182652fba9f396660baf
Andreas Heinrich <git@heinrian.com>
2024-11-16 02:21:48 +0100

    add error message for player error

--- src/main/java/de/bitwars/business_logic/moduels/player/RemotePlayer.java
+++ src/main/java/de/bitwars/business_logic/moduels/player/RemotePlayer.java
@@ -68,7 +68,7 @@ public class RemotePlayer implements ActionProvider {
 
             return response.stream().map(playerAction -> gameBUMapper.toPlayerActionBU(playerAction)).toList();
         } catch (final Exception e) {
-            log.error("[{}] Error while requesting bet from player {}", gameBU.getId(), url);
+            log.error("[{}] Error while requesting bet from player {}: {}", gameBU.getId(), url, e.getMessage());
         }
         return List.of();
     }
